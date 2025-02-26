c39f4fdbbb1d5db556a4ad994b7ce0663ea0fac9
Cielito <cielitomelga7@gmail.com>
2025-02-25 21:36:11 -0300

    Prueba6

---
+++ 2. Pruebas y ejercicios/Prueba6/src/paquete1/Atributos_Metodos.java
@@ -0,0 +1,15 @@
+/*
+ * Solamente java uno tendra acceso a las variables por default, porque son 
+ * las que se encuentran en el mismo paquete.
+ */
+package paquete1;
+
+/**
+ *
+ * @author Cielito
+ */
+public class Atributos_Metodos {
+    int valorUno;
+    int valorDos;
+    String valorTres;
+}
