a09f86a2db08cda957a460be990132bfbdba927a
Cielito <cielitomelga7@gmail.com>
2025-02-28 20:53:18 -0300

    Prueba9

---
+++ 2. Pruebas y ejercicios/Prueba9/src/paquete1/Atributos_Metodos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete1;
+
+/**
+ *
+ * @author Cielito
+ */
+public class Atributos_Metodos {
+    protected int valorUno;
+    protected int valorDos;
+    protected String valorTres;
+    
+    protected void metodoMaria(){
+        
+    }
+
+}
---
+++ 2. Pruebas y ejercicios/Prueba9/src/paquete2/ClaseJavaDOs.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete2;
+
+import paquete1.Atributos_Metodos;
+
+/**
+ *
+ * @author Cielito
+ */
+public class ClaseJavaDOs {
+    public static void main(String[] args){
+         Atributos_Metodos mensajero = new Atributos_Metodos();
+         //No se puede acceder a ningun metodo, ni atributo, porque estan en otro paquete y en modificador protected
+       
+    }
+}
