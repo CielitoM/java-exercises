af6f43779f6614826f3a9a9c432b2935b82cbf9c
Cielito <cielitomelga7@gmail.com>
2025-03-08 22:44:07 -0300

    Continuacion de prueba14

--- 2. Pruebas y ejercicios/Prueba14/src/paquete1/ClasePadre_Abstracta.java
+++ 2. Pruebas y ejercicios/Prueba14/src/paquete1/ClasePadre_Abstracta.java
@@ -14,17 +14,36 @@ public class ClasePadre_Abstracta {
      public void Operaciones(){
          int bandera = 0;
          int seleccion = 0;
+         do{
+            do{
+               System.out.println("Por favor, seleccione una opción: ");
+               System.out.println("    1. Consulta saldo.");
+               System.out.println("    2. Retir de efectivo.");
+               System.out.println("    3. Deposito de efectivo.");
+               System.out.println("    4. Salir.");
+               seleccion = entrada.nextInt();
+
+               if(seleccion >= 1 && seleccion <=4){
+                   bandera = 1;   
+               }else{
+                   System.out.println("-------------------------------------------------");
+                   System.out.println("Opcion no disponible, vuelva a intentar por favor");
+                   System.out.println("-------------------------------------------------");
+                   }  
+               }while(bandera == 0);
+            
+         if(seleccion == 1){
+         
+         }else if(seleccion == 2){
+         
+         }else if(seleccion == 3){
+         
+         }else if(seleccion == 4){
+             System.out.println("-----------------------");
+             System.out.println("Gracias, vuelva pronto!");
+             System.out.println("-----------------------");
+         } 
+           
+         }while(bandera !=2);
      }
-    
-        public void seleccionar_Opcion(){
-        System.out.println("Por favor, seleccione una opción: ");
-        System.out.println("1. Consulta saldo.");
-        System.out.println("2. Retir de efectivo.");
-        System.out.println("3. Deposito de efectivo.");
-        System.out.println("4. Salir.");
-        seleccion = entrada.nextInt();
-    }
-    
-    
-    
 }
