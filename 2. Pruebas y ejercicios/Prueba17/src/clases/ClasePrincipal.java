eb8f9f1d13f6767b3aa886506a73cf8fff97f2d6
Cielito <cielitomelga7@gmail.com>
2025-03-15 22:50:41 -0300

    Prueba 17

--- 2. Pruebas y ejercicios/Prueba17/src/clases/ClasePrincipal.java
+++ 2. Pruebas y ejercicios/Prueba17/src/clases/ClasePrincipal.java
@@ -6,14 +6,17 @@ package clases;
  */
 public class ClasePrincipal {
     public static void main(String[] args){
-        Proceso hilo1 = new Proceso();
-        Proceso hilo2 = new Proceso();
+        Proceso hilo1 = new Proceso(" Hilo 1");
+        Proceso hilo2 = new Proceso(" Hilo 2");
+        Proceso hilo3 = new Proceso(" Hilo 3");
         
         //pasar los paramtros
         hilo1.ValorDeLaCondicion(5);
         hilo2.ValorDeLaCondicion(10);
+        hilo3.ValorDeLaCondicion(5);
         
         hilo1.start();
         hilo2.start();
+        hilo3.start();
     }
 }
--- 2. Pruebas y ejercicios/Prueba17/src/clases/Proceso.java
+++ 2. Pruebas y ejercicios/Prueba17/src/clases/Proceso.java
@@ -8,10 +8,14 @@ public class Proceso extends Thread{
     
     int num_int;
     
+    public Proceso(String NombreHilo){
+        super(NombreHilo);
+    }
+    
     @Override
     public void run(){
         for(int i = 0; i<= num_int; i++){
-            System.out.println(i);
+            System.out.println(i + this.getName());
         }
         System.out.println("");
     }
