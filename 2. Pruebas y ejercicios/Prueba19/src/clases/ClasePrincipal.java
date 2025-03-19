06f280f79318e4f988a2ba26e703ec906543f9d3
Cielito <cielitomelga7@gmail.com>
2025-03-18 21:38:01 -0300

    Prueba19

---
+++ 2. Pruebas y ejercicios/Prueba19/src/clases/ClasePrincipal.java
@@ -0,0 +1,24 @@
+package clases;
+
+/**
+ *
+ * @author Cielito
+ */
+public class ClasePrincipal {
+    public static void main(String[] args){
+        Hilo_Proceso hilo1 = new Hilo_Proceso();
+        Hilo_Proceso hilo2 = new Hilo_Proceso();
+        
+        hilo1.start();
+        try{
+          hilo1.sleep(1000);  
+        }catch(InterruptedException e){
+            System.out.println("Error en el hilo 1" + e);
+        }
+        
+        
+        hilo2.start();
+        
+        
+    }
+}
