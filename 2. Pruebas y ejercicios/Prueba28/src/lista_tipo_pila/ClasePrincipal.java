b7da7bbcf225190abbeac236d9507fd3970a1d11
Cielito <cielitomelga7@gmail.com>
2025-03-29 22:25:00 -0300

    Prueba28 - terminado

--- 2. Pruebas y ejercicios/Prueba28/src/lista_tipo_pila/ClasePrincipal.java
+++ 2. Pruebas y ejercicios/Prueba28/src/lista_tipo_pila/ClasePrincipal.java
@@ -1,9 +1,86 @@
 package lista_tipo_pila;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Cielito
  */
 public class ClasePrincipal {
-    
+    public static void main(String[] args) {
+        //Variables
+        int opcion = 0, nodo = 0;
+        Pila pila = new Pila();
+        
+        //Menu de opciones
+        do{
+            try{
+                
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "Menu de Opciones\n\n"
+                        + "1. Insertar un nodo\n"
+                        + "2. Eliminar un nodo\n"
+                        + "3. ¿La pila esta vacia?\n"
+                        + "4. ¿Cual es el ultimo valor ingresado en la Pila\n"
+                        + "5. ¿Cuantos nodos tiene la pila?\n"
+                        + "6. Vaciar Pila\n"
+                        + "7. Monstrar contenido de la pila\n"
+                        + "8. Salir\n\n"));
+            
+                    switch(opcion){
+                        case 1:
+                            nodo = Integer.parseInt(JOptionPane.showInputDialog(null, "Por favor, ingresa el valor a guardar en el nodo"));
+                            pila.InsertarNodo(nodo);
+                            break;
+                        case 2:
+                            if(!pila.PilaVacia()){
+                                JOptionPane.showMessageDialog(null, "Se ha eliminado un nodo con el valor: "
+                                + pila.EliminarNodo());
+                            }else{
+                                JOptionPane.showMessageDialog(null, "La pila esta vacia.");
+                            }
+                            break;
+                        case 3:
+                            if(pila.PilaVacia()){
+                                JOptionPane.showMessageDialog(null, "La pila esta vacia.");
+                            }else{
+                                JOptionPane.showMessageDialog(null, "La pila no esta vacia.");
+                            }
+                            break;
+                        case 4:
+                            if(!pila.PilaVacia()){
+                                JOptionPane.showMessageDialog(null, "El ultimo valor ingresado en la pila es: "
+                                + pila.MostrarUltimoValorIngresado());
+                            }else{
+                                JOptionPane.showMessageDialog(null, "La pila esta vacia.");
+                            }
+                            break;
+                        case 5:
+                            JOptionPane.showMessageDialog(null, "La pila contiene " + pila.TamanoPila() + " nodos.");
+                            break;
+                        case 6:
+                            if(!pila.PilaVacia()){
+                                pila.VaciarPila();
+                                JOptionPane.showMessageDialog(null, "Se ha vaciado la pila correctamente.");
+                            }else{
+                                JOptionPane.showMessageDialog(null, "La pila esta vacia.");
+                            }
+                            break;
+                        case 7:
+                            pila.MostrarValores();
+                            break;
+                        case 8:
+                            opcion = 8;
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "Opcion incorrecta, vuelve a intentarlo nuevamente.");
+                            break;
+                    }
+                
+            }catch(NumberFormatException e){
+                
+            }
+            
+        }while(opcion != 8);
+    }
 }
--- 2. Pruebas y ejercicios/Prueba28/src/lista_tipo_pila/Pila.java
+++ 2. Pruebas y ejercicios/Prueba28/src/lista_tipo_pila/Pila.java
@@ -1,9 +1,71 @@
 package lista_tipo_pila;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Cielito
  */
 public class Pila {
+    //Atributos de la clase
+    private Nodo UltimoValorIngresado;
+    int tamano = 0;
+    String Lista = "";
     
+    //Constructor
+    public Pila(){
+        UltimoValorIngresado = null;
+        tamano = 0;
+    }
+    
+    //Metodo para conocer cuando la pila esta vacia
+    public boolean PilaVacia(){
+        return UltimoValorIngresado == null;
+    }
+    
+    //Metodo para insertar un nodo en la pila
+    public void InsertarNodo(int nodo){
+        Nodo nuevo_nodo = new Nodo(nodo);
+        nuevo_nodo.siguiente =  UltimoValorIngresado;
+        UltimoValorIngresado = nuevo_nodo;
+        tamano++;
+    }
+    
+    //Metodo para eliminar un nodo de la pila
+    public int EliminarNodo(){
+        int auxiliar = UltimoValorIngresado.informacion;
+        UltimoValorIngresado = UltimoValorIngresado.siguiente;
+        tamano--;
+        return auxiliar;
+    }
+
+    //Metodo para conocer cual es el ultimo valor ingresado
+    public int MostrarUltimoValorIngresado(){
+        return UltimoValorIngresado.informacion;
+    }
+    
+    //Metodo para conocer el tamaño de la Pila
+    public int TamanoPila(){
+        return tamano;
+    }
+
+    //Metodo para vaciar la Pila
+    public void VaciarPila(){
+        while (!PilaVacia()) {
+            EliminarNodo();
+        }
+    }
+        
+    //Metodo para mostrar el contenido de la pila
+    public void MostrarValores(){
+        Nodo recorrido = UltimoValorIngresado;
+        
+        while(recorrido != null){
+            Lista += recorrido.informacion + "\n";
+            recorrido = recorrido.siguiente;
+        }
+        JOptionPane.showMessageDialog(null, Lista);
+        Lista = "";
+    }
 }
+
