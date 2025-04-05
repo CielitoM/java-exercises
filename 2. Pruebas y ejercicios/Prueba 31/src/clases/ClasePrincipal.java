fcee8a4201aab24f3a2f5463f96ed18e3e74eceb
Cielito <cielitomelga7@gmail.com>
2025-04-04 22:19:24 -0300

    Prueba31

---
+++ 2. Pruebas y ejercicios/Prueba 31/src/clases/ClasePrincipal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Cielito
+ */
+public class ClasePrincipal {
+    public static void main(String[] args) {
+        Recursividad mensajero = new Recursividad();
+        mensajero.Imprimir(0); //Que empiece desde el 0
+    }
+   
+    
+}
---
+++ 2. Pruebas y ejercicios/Prueba 31/src/clases/Recursividad.java
@@ -0,0 +1,14 @@
+package clases;
+
+/**
+ *
+ * @author Cielito
+ */
+public class Recursividad {
+    public void Imprimir(int x){
+        if(x<=5){
+            System.out.println(x + " ");
+            Imprimir(x+1);
+        }
+    }
+}
